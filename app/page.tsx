1f0649e55c767996e518a43d5a8353913ca90135
Oleksandr Shendryk <oleksandr.shendryk.ua@gmail.com>
2026-03-16 16:55:22 +0200

    feat: auto-redirect root to Ukrainian page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,22 @@
-import Link from "next/link";
+"use client";
 
-import { defaultLocale } from "@/lib/i18n";
+import { useEffect } from "react";
 
 export default function HomePage() {
+  useEffect(() => {
+    window.location.replace("./uk/");
+  }, []);
+
   return (
     <main className="container" style={{ paddingTop: "6rem", paddingBottom: "2rem" }}>
       <section className="block">
-        <h1 style={{ margin: 0, fontFamily: "Georgia, serif" }}>Pilates Studio</h1>
-        <p style={{ marginTop: "0.7rem" }}>
-          Choose language:
-          <Link href={`/${defaultLocale}`} style={{ marginLeft: "0.5rem", color: "#2f6a66" }}>
-            {defaultLocale.toUpperCase()}
-          </Link>
-          <Link href="/en" style={{ marginLeft: "0.8rem", color: "#2f6a66" }}>
-            EN
-          </Link>
+        <p>
+          Redirecting to Ukrainian page...
+          <a href="./uk/" style={{ marginLeft: "0.6rem", color: "#2f6a66" }}>
+            Open now
+          </a>
         </p>
       </section>
     </main>
   );
 }
-
