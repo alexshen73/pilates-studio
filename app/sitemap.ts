a000e353ac0b9fdaba1593d64df84cfd8fa7574b
alexshen73 <alexshen73@gmail.com>
2026-06-10 16:57:54 +0300

    Add blog pages to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,13 +1,14 @@
 import type { MetadataRoute } from "next";
 
+import { content } from "@/lib/content";
+
 export const dynamic = "force-static";
 
 const baseUrl = "https://www.pilates-yuliya.com";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const now = new Date();
-
-  return [
+  const pages: MetadataRoute.Sitemap = [
     {
       url: `${baseUrl}/uk`,
       lastModified: now,
@@ -28,6 +29,50 @@ export default function sitemap(): MetadataRoute.Sitemap {
         },
       },
     },
+    {
+      url: `${baseUrl}/uk/blog`,
+      lastModified: now,
+      alternates: {
+        languages: {
+          uk: `${baseUrl}/uk/blog`,
+          en: `${baseUrl}/en/blog`,
+        },
+      },
+    },
+    {
+      url: `${baseUrl}/en/blog`,
+      lastModified: now,
+      alternates: {
+        languages: {
+          uk: `${baseUrl}/uk/blog`,
+          en: `${baseUrl}/en/blog`,
+        },
+      },
+    },
   ];
+
+  const ukPosts = content.uk.blog.posts.map((post, index) => ({
+    url: `${baseUrl}/uk/blog/${post.slug}`,
+    lastModified: now,
+    alternates: {
+      languages: {
+        uk: `${baseUrl}/uk/blog/${post.slug}`,
+        en: `${baseUrl}/en/blog/${content.en.blog.posts[index]?.slug ?? ""}`,
+      },
+    },
+  }));
+
+  const enPosts = content.en.blog.posts.map((post, index) => ({
+    url: `${baseUrl}/en/blog/${post.slug}`,
+    lastModified: now,
+    alternates: {
+      languages: {
+        uk: `${baseUrl}/uk/blog/${content.uk.blog.posts[index]?.slug ?? ""}`,
+        en: `${baseUrl}/en/blog/${post.slug}`,
+      },
+    },
+  }));
+
+  return [...pages, ...ukPosts, ...enPosts];
 }
 
